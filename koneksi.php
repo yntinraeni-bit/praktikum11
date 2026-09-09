9b03daa3dfabdcee135ae3b46a818771d98c086a
yntinraeni-bit <yantikosongdua4@gmail.com>
2026-09-08 21:35:54 -0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "praktikum11");
-
-if (!$koneksi) {
-    die("Koneksi database gagal");
-}
-
-define('BASE_PATH', __DIR__);
-define('BASE_URL', '/praktikum11');
